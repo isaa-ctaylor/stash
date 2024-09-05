46948b4a3f178645c3a6d1de3d6a4432bc03bbac
isaa-ctaylor <isaac@isaactaylor.xyz>
2024-09-06 00:39:06 +0100

    feat: :lipstick: add copy button

--- stash/src/js/stash.js
+++ stash/src/js/stash.js
@@ -1,24 +1,31 @@
-document.getElementById("submit").onclick = async (evt) => {
-    evt.preventDefault();
-
-    const password = document.getElementById("password").value.trim();
-
-    if (password.length === 0) {
-        alert("Password cannot be empty");
-        return;
+const submitButton = document.getElementById("submit");
+if (submitButton) {
+    submitButton.onclick = async (evt) => {
+        evt.preventDefault();
+    
+        const password = document.getElementById("password").value.trim();
+    
+        if (password.length === 0) {
+            alert("Password cannot be empty");
+            return;
+        }
+    
+        fetch(window.location.pathname + "?" + new URLSearchParams({ raw: 1 }).toString(), {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + password
+            }
+        }).then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+        }).then((data) => {
+            document.getElementById("content").innerHTML = data.content;
+            document.getElementById("password-form").style.display = "none";
+        });
     }
+}
 
-    fetch(window.location.pathname + "?" + new URLSearchParams({ raw: 1 }).toString(), {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + password
-        }
-    }).then((response) => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).then((data) => {
-        document.getElementById("content").innerHTML = data.content;
-        document.getElementById("stash-password-input").style.display = "none";
-    });
+document.getElementById("copy-button").onclick = () => {
+    navigator.clipboard.writeText(document.getElementById("content").textContent.trim());
 }
