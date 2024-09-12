b93792fcb476fc4c3b38e68a1830261d712357b3
isaa-ctaylor <isaac@isaactaylor.xyz>
2024-09-12 01:27:40 +0100

    feat: :sparkles: add oauth logic

--- stash/__main__.py
+++ stash/__main__.py
@@ -1,20 +1,27 @@
 import base64
+import datetime
+import os
 import pathlib
 
+import bcrypt
 import cryptography
 import cryptography.exceptions
+import jwt
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, Request, staticfiles
 from fastapi.responses import FileResponse, JSONResponse
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+
+load_dotenv()
 
 __version__ = "0.0.1"
 
@@ -22,7 +29,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
+SECRET_KEY = os.getenv("SECRET_KEY")
+print(SECRET_KEY)
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRES_MINUTES = 30
+CREDENTIALS_EXCEPTION = HTTPException(
+    status_code=401,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
 
 wd = pathlib.Path(__file__).parent.resolve()
 
@@ -110,7 +126,7 @@ def get_stash_id_from_request(request: Request):
 def resolve_stash(
     request: Request,
     db: Session = Depends(get_db),
-    token: str | None = Depends(oauth2_scheme),
+    token: str | None = Depends(oauth_scheme),
     stash_id: str = Depends(get_stash_id_from_request),
     raw: bool | None = None,
 ):
@@ -130,6 +146,80 @@ def resolve_stash(
         return stash
 
     raise HTTPException(status_code=404, detail="Page not found")
+
+
+def decode_token(token: str = Depends(oauth_scheme), db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(username=username)
+    except jwt.InvalidTokenError:
+        raise CREDENTIALS_EXCEPTION
+
+    user = crud.get_user_by_email(db, email=token_data.username)
+    return user
+
+
+async def get_current_user(user: schemas.User | None = Depends(decode_token)):
+    if not user:
+        raise CREDENTIALS_EXCEPTION
+    return user
+
+
+async def get_current_active_user(
+    current_user: schemas.User = Depends(get_current_user),
+):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+def create_access_token(data: dict, expires_delta: datetime.timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.datetime.now(datetime.timezone.utc) + expires_delta
+    else:
+        expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+            minutes=30
+        )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user: models.User = crud.get_user_by_email(db, email=form_data.username)
+    if not user:
+        raise CREDENTIALS_EXCEPTION
+    if not bcrypt.checkpw(bytes(form_data.password, "utf-8"), user.hashed_password):
+        raise CREDENTIALS_EXCEPTION
+    access_token_expires = datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return schemas.Token(access_token=access_token, token_type="bearer")
+
+
+@app.get("/admin")
+def admin(
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_active_user),
+):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return "admin"
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: models.User = Depends(get_current_active_user),
+):
+    return current_user
 
 
 @app.get("/{stash_id}")
