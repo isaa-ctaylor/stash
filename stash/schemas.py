a3935b5a5bf125eb419abf1b061b41a8260834d2
isaa-ctaylor <isaac@isaactaylor.xyz>
2024-09-12 01:27:56 +0100

    feat: :sparkles: add token schemas

--- stash/schemas.py
+++ stash/schemas.py
@@ -39,3 +39,12 @@ class User(UserBase):
 
     class Config:
         from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
